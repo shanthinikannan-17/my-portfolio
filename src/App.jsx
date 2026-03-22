3a8097b57bbc1712e39da29d8e90c08c5573a22d
Shanthini Kannan <shanthinikannan7@gmail.com>
2026-03-22 23:37:32 +0430

    fix star background import

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Education from "./components/Education"
 import Contact from "./components/Contact"
 import Footer from "./components/Footer"
 import Interests from "./components/Interests"
-import StarBackground from "./components/Starbackground"
+import StarBackground from "./components/StarBackground"
 function App() {
   return (
     <>
